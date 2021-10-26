8444900f0979bf05f7c5f800dfe2b6c87e568872
lpng-plenge <luispablonieto@gmail.com>
2021-10-26 00:39:16 -0500

    City Resize

--- src/funny/Funny.java
+++ src/funny/Funny.java
@@ -1,30 +1,22 @@
 package funny;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class Funny extends JFrame {
 
     public boolean thbool = true; //hilo
-    public boolean fBool = true; //hilo
+    public boolean fBool = true; //Fondo
+    public boolean edBool = true; //edificios
     public Image fondo;
-    public Image buffer;
     public BufferedImage bufferedImage;
     public Graphics graPixel;
-
     //colores
     public int greenFondo = 255, blueFondo = 0, redFondo = 255;//fondo
     //edificios posiciones {x,y}
-    public int dimensionesEd[][] = {{35, 45, 25, 15, 30}, {125, 45, 75, 200, 15}};
+    public int dimensionesEd[][] = new int[12][12];
+    public int xEdificios = 0;
 
     public static void main(String[] args) {
         new Funny().setVisible(true);
@@ -48,7 +40,6 @@ public class Funny extends JFrame {
         Dimension dimWindow = myWindow.getScreenSize();
         this.setSize(dimWindow.width / 2, dimWindow.height / 2);
         this.setLocation(dimWindow.width / 4, dimWindow.height / 4);
-
     }
 
     public class Hilos extends Thread implements Runnable {
@@ -73,6 +64,7 @@ public class Funny extends JFrame {
             }
         }
 
+        //Cambiar COlor de Fondo
         public void fondoIncrese() {
             if (greenFondo <= 105) {
                 redFondo--;
@@ -95,31 +87,46 @@ public class Funny extends JFrame {
 
         public void paint(Graphics g) {
             super.paint(g);
-//            if(fondo==null){
+
             fondo = createImage(getWidth(), getHeight());
             int alto = fondo.getHeight(this);
             int largo = fondo.getWidth(this);
-
+            
             //asignarle los graficos 2D con el bufer de graphics
             graPixel = (Graphics2D) g;
+
+            fondo(alto, largo);
+            ciudad(alto, largo);
+
+            update(g);
+        }
+
+        public void fondo(int alto, int largo) {
             Color color = new Color(redFondo, greenFondo, blueFondo);
             graPixel.setColor(color);
             graPixel.fillRect(0, 0, largo, alto);
+        }
 
+        public void ciudad(int alto, int largo) {
             //variables de los edificios
-            int xEdificios = 0, cantidadEdificios = dimensionesEd[0].length, altoEd=0;
+            int cantidadEdificios = dimensionesEd[0].length;
             int edPos = 0, edY = 0, edX = 0;
             graPixel.setColor(Color.BLACK);//color
 
             //calcular cantidad de edificios por el layout
-            for (int i = 0; i < cantidadEdificios; i++) {
-                xEdificios += dimensionesEd[0][i];
-                //altoEd = 15 + (int) (Math.random() * (alto * .5));
-                //dimensionesEd[1][i]+= altoEd;
+            if (edBool) {
+                for (int i = 0; i < cantidadEdificios; i++) {
+                    int largoEd = 15 + (int) (Math.random() * (largo * 0.055));
+                    int altoEd = 15 + (int) (Math.random() * (alto * .35));
+                    dimensionesEd[0][i] += largoEd;
+                    dimensionesEd[1][i] += altoEd;
+                    xEdificios += dimensionesEd[0][i];
+                }
+                edBool = false;
             }
-            int jFrameEdificios = Math.abs(largo / xEdificios) * cantidadEdificios;
 
             //dibujar edificios
+            int jFrameEdificios = Math.abs(largo / xEdificios) * cantidadEdificios;
             for (int i = 0; i < jFrameEdificios; i++) {
                 if (edPos > cantidadEdificios - 1) {
                     edPos = 0;
@@ -129,9 +136,6 @@ public class Funny extends JFrame {
                 edX += dimensionesEd[0][edPos];
                 edPos++;
             }
-            //g.drawImage(bufferedImage, 0, 0, this);
-
-            update(g);
         }
 
         public void update(Graphics g) {
